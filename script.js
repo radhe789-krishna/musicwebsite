5c760e5f9594154a7ae8dcb64bc7c247ba84f39b
radhe789-krishna <128914965+radhe789-krishna@users.noreply.github.com>
2024-02-22 22:02:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello");
+let audioElement =new Audio('1.mp3');
+audioElement.play();
+for (let i = 0; i < 5; i++) {
+    console.log("hare krishna");
+
+    
+}
+let str='i love krishna';
+let obj = {
+    Name:'kirshan',
+    caste:'hemani',
+    village:'qadirpur'
+};
+console.log("my name is =", obj.Name,'my caste =', obj.caste,'and my village =', obj.village);
